5ce5ccf965a6b7310e70cdcba9a907f0ec7b19a9
Shivam chauhan <144655342+shivamch023@users.noreply.github.com>
2024-03-01 02:48:46 -0800

    shivam

--- src/component/WeatherComponent.jsx
+++ src/component/WeatherComponent.jsx
@@ -100,6 +100,7 @@ const WeatherComponent = () => {
         <div className='w-full h-screen flex items-center justify-center'>
           <h1 className='text-6xl font-bold'>Loading weather data...</h1>
           <p>Shivam Enter...</p>
+          <h1>moin gandu</h1>
         </div>
       )}
     </div>
